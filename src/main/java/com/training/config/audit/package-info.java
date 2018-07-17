b342569d3328920df63fbaa29f8fac19db482b7b
Souheil MAALAOUI <maalaoui.ensi@gmail.com>
2018-07-17 02:13:19 -0700

    Initial application generated by JHipster-5.1.0

---
+++ src/main/java/com/training/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.training.config.audit;
---
+++ src/main/java/com/training/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.training.repository;
+
+import com.training.domain.Authority;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the Authority entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {
+}
